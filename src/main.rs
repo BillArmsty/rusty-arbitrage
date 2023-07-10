c78b75b576430c080b027c5bcbf705002fd63ca2
Bill Armstrong <armsty9@gmail.com>
2023-07-10 14:36:57 +0300

    Implemented the tick to price and liquidity0

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use rand::Rng;
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::RwLock;
+use std::thread;
+use std::time::Duration;
+
+mod math;
+
+#[derive(PartialEq, Copy, Clone)]
+enum Token {
+    Eth,
+    Dai
+}
+
+fn price_to_tick(price: f64) -> f64 {
+    price.log(1.001).floor()
+}
+
+fn tick_to_price(tick: i32) -> f64 {
+    let base: f64 = 1.001;
+    let num: f64 = base.powi(tick);
+    num.sqrt() * math::get_q96()
 }
 
 
+fn price_to_sqrtp(price: f64) -> f64 {
+    price.sqrt() * math::get_q96()
+}
+
+fn liquidity0(amount: f64, pa: f64, p)
